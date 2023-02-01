565c1f816a4fb66efe3299c6702d2ee83edc197a
Andrew Owens <andrrowens@gmail.com>
2023-01-31 17:58:03 -0700

    commit tree 7219eaa4890e00c80c56516e84d067debb812c46

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,10 @@
 class Author < ApplicationRecord
+    #validates all authors have a name
+    validates :name, presence: true
+
+    #validates no two authors have the same name
+    validates :name, uniqueness: {case_sensative: true}
+
+    #validates that phone number length is 10 numbers
+    validates :phone_number, length: {is: 10, message: "%{value} should be 10 digits!"}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,30 @@
 class Post < ApplicationRecord
+
+    #Object lifecycle callback
+    before_validation :capitalize_category_name
+
+    validates :title, presence: true
+
+    validates :content, length: { minimum: 250 }
+
+    validates :summary, length: { maximum: 250 }
+
+
+    validates :category, inclusion: {case_sensitive: false, in: ["Fiction", "Non-Fiction"]} 
+
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+
+    validate :true_facts
+
+    valiates :title, format: {with: /Won't Believe|Secret|Top \d|Guess/i}
+
+    def true_facts
+       if  [/Won't Believe/i, /Secret/i.non {|pattern| pattern.match(self.title)}
+
+
+
+    def capitalize_category_name 
+        self.category = self.category.capitalize 
+    end
+
 end
